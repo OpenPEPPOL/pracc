396eccbfa53cf57974e612a131e4bbeb93996c64
Schömer, Jörg <schoemer@adesso.de>
2020-10-14 00:29:02 +0200

    show content of "$FOLDER/target/site/files/schematrons.zip"

--- tools/bin/build.sh
+++ tools/bin/build.sh
@@ -30,9 +30,10 @@ info "Run vefa-structure"
 dc structure
 
 info "Create ZIP file with schematrons"
-find "$FOLDER/target/site/files"
-mkdir -p "$FOLDER/target/site/files"
-zip -r "$FOLDER/target/site/files/schematrons.zip" "$FOLDER/rules/"
+find "$FOLDER/target/site/files" -ls
+zipinfo "$FOLDER/target/site/files/schematrons.zip"
+zip -r "$FOLDER/target/site/files/schematrons-1.zip" "$FOLDER/rules/"
+mv "$FOLDER/target/site/files/schematrons-1.zip" "$FOLDER/target/site/files/schematrons.zip"
 
 info "Build and verify validation artifacts"
 dc validator
